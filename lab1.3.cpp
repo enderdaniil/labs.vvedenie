4f4e3bd678b7dcf0624d3d0358435f57a5555e3c
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-09-05 22:18:54 +0300

    Create lab1.3.cpp

---
+++ lab1.3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float a,b,s;
+    cout<<"Input a and b"<<endl;
+    cin>>a>>b;
+    s=(a+b)/2;
+    cout<<"Srednee arifm="<<s<<endl;
+    return 0;
+}
