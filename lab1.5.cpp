21d913750214ad4240657fbd960fb7db461c49fa
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-09-05 22:21:08 +0300

    Create lab1.5.cpp

---
+++ lab1.5.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    float sum,proizv,chast,a,b,raz;
+    cout<<"Input chisla"<<endl;
+    cin>>a>>b;
+    a=abs(a);
+    b=abs(b);
+    sum=a+b;
+    if (a>b){
+    raz=a-b;}
+    else {raz=b-a;}
+    proizv=a*b;
+    chast=a/b;
+    cout<<"chastnoe="<<chast<<endl<<"raznost="<<raz<<endl<<"proizv="<<proizv<<endl<<"summ="<<sum<<endl;
+    return 0;
+}
